c2d3e9e29c4ca7d59204ba3e5d041f1f05866aa4
renanbmx123 <renanbmx123@gmail.com>
2022-12-18 01:16:02 -0300

    adicionado o pluguin harpoon

--- lua/renan/packer.lua
+++ lua/renan/packer.lua
@@ -18,5 +18,5 @@ return require('packer').startup(function(use)
 
   use( 'nvim-treesitter/nvim-treesitter', {run =  ':TSUpdate'})
   use 'nvim-treesitter/playground'
-
+  use 'ThePrimeagen/harpoon'
 end)
