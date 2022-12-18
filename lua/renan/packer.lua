2d98759a26b5f2faf358b7f51b746f29cd66c7c2
renanbmx123 <renanbmx123@gmail.com>
2022-12-18 01:07:39 -0300

    Ajuste no esquema de cores, e adição do plugin tressiter

--- lua/renan/packer.lua
+++ lua/renan/packer.lua
@@ -15,4 +15,8 @@ return require('packer').startup(function(use)
 		  vim.cmd('colorscheme rose-pine')
 	  end
   })
+
+  use( 'nvim-treesitter/nvim-treesitter', {run =  ':TSUpdate'})
+  use 'nvim-treesitter/playground'
+
 end)
